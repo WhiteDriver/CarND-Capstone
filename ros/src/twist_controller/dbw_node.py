86d864eebd8a3ca8ab82b7a64b32632e439fcc7e
vishnerevsky <31100028+vishnerevsky@users.noreply.github.com>
2017-08-24 08:23:50 -0700

    reworked dbw_node

--- ros/src/twist_controller/dbw_node.py
+++ ros/src/twist_controller/dbw_node.py
@@ -6,11 +6,11 @@ from dbw_mkz_msgs.msg import ThrottleCmd, SteeringCmd, BrakeCmd, SteeringReport
 from geometry_msgs.msg import TwistStamped, PoseStamped
 from styx_msgs.msg import Lane, Waypoint
 import math
+import numpy as np
 
-from twist_controller import Controller
-from yaw_controller import YawController
 from pid import PID
 import time
+
 
 '''
 You can build this node only after you have built (or partially built) the `waypoint_updater` node.
@@ -49,10 +49,25 @@ class DBWNode(object):
         steer_ratio = rospy.get_param('~steer_ratio', 14.8)
         max_lat_accel = rospy.get_param('~max_lat_accel', 3.)
         max_steer_angle = rospy.get_param('~max_steer_angle', 8.)
-        min_speed = 0.1
-        kp = 1
-        ki = 0.05
-        kd = 0
+
+        # Some important variables:
+        self.current_pose = None
+        self.final_waypoints = None
+
+        # Another important variables:
+        self.cur_v = 0.0
+        # Valtgun 20.08.2017 - subscribe to is DBW enabled topic
+        self.dbw_enabled = False
+        rospy.Subscriber('/vehicle/dbw_enabled', Bool, self.dbw_cb)
+
+        # Vishnerevsky 22.08.2017 - timestamps for PID controller
+        self.previous_timestamp = rospy.get_rostime().secs
+        self.current_timestamp = 0.0
+        self.delta_time = 0.0
+        
+        # Vishnerevsky 24.08.2017
+        self.V_pid = PID(kp=0.4, ki=0.0, kd=0, mn=decel_limit, mx=accel_limit)
+        self.S_pid = PID(kp=0.2, ki=0.001, kd=0.5)
 
         self.steer_pub = rospy.Publisher('/vehicle/steering_cmd',
                                          SteeringCmd, queue_size=1)
@@ -62,50 +77,19 @@ class DBWNode(object):
                                          BrakeCmd, queue_size=1)
 
         # TODO: Create `TwistController` object
-        # self.controller = TwistController(<Arguments you wish to provide>)
-        self.controller = Controller(max_st_angle = max_steer_angle)
-        
-        # Vishnerevsky 22.08.2017
-        self.yaw_contr = YawController(wheel_base, steer_ratio, min_speed, max_lat_accel, max_steer_angle)
-        self.pid_contr = PID(kp, ki, kd)
+        #self.controller = Controller(self.throttle_pid, self.steering_pid)
 
         # TODO: Subscribe to all the topics you need to
-        # Valtgun 20.08.2017 - subscribe to is DBW enabled topic
-        self.dbw_enabled = False
+        #rospy.Subscriber('/twist_cmd', TwistStamped, self.twist_commands_cb)
         rospy.Subscriber('/vehicle/dbw_enabled', Bool, self.dbw_cb)
-
-        # Valtgun 20.08.2017 - subscribe to next waypoints
-        rospy.Subscriber('/final_waypoints', Lane, self.waypoints_cb)
-        # target X,Y coordinates for simple waypoint lookup
-        self.targetX = 0.0
-        self.targetY = 0.0
-
-        # Vishnerevsky 22.08.2017 - target speed and yaw
-        self.target_speed = 0.0
-        self.target_yaw = 0.0
-        self.cur_v = 0.0
-        # Vishnerevsky 22.08.2017 - timestamps for PID controller
-        self.previous_timestamp = rospy.get_rostime().secs
-        self.current_timestamp = 0.0
-        self.delta_time = 0.0
-
-        # Valtgun 20.08.2017 - subscribe to current pose
-        rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
-        self.poseX = 0.0
-        self.poseY = 0.0
-        self.pose_orient = 0.0 # after conversion to euler
-        # values in degrees (-180 to +180)
-        # 0 aligned with X axis (in front of car initailly)
-        # +90 is to the left of inital car position
-
-        # Vishnerevsky 22.08.2017 - subscribe to /twist_cmd
-        rospy.Subscriber('/twist_cmd', TwistStamped, self.twist_cb)
-        rospy.Subscriber('/current_velocity', TwistStamped, self.cur_vel_cb)
+        rospy.Subscriber('/current_velocity', TwistStamped, self.cur_vel_cb, queue_size=1)
+        rospy.Subscriber('/current_pose', PoseStamped, self.current_pose_cb, queue_size=1)
+        rospy.Subscriber('/final_waypoints', Lane, self.final_waypoints_cb)
 
         self.loop()
 
     def loop(self):
-        rate = rospy.Rate(50) # 50Hz
+        rate = rospy.Rate(10) # 50Hz
         while not rospy.is_shutdown():
             # TODO: Get predicted throttle, brake, and steering using `twist_controller`
             # You should only publish the control commands if dbw is enabled
@@ -114,26 +98,45 @@ class DBWNode(object):
             #                                                     <current linear velocity>,
             #                                                     <dbw status>,
             #                                                     <any other argument you need>)
-            throttle, brake, steer = self.controller.control(dbw = self.dbw_enabled,
-                                                            tx = self.targetX,
-                                                            ty = self.targetY,
-                                                            px = self.poseX,
-                                                            py = self.poseY,
-                                                            pt = self.pose_orient)
-            # Vishnerevsky 22.08.2017:
-            steer = self.yaw_contr.get_steering(self.target_speed, self.target_yaw, self.cur_v) 
-            current_time = rospy.get_rostime()
-            current_secs = current_time.secs
-            current_nsecs = current_time.nsecs
-            self.current_timestamp = current_secs + current_nsecs/1000000000.0
-            self.delta_t = (self.current_timestamp - self.previous_timestamp)
-            self.previous_timestamp = self.current_timestamp
-            rospy.logwarn(self.delta_t)
-            PID_speed = self.pid_contr.step(self.target_speed - self.cur_v, self.delta_t)
-            if self.dbw_enabled:
-                #self.publish(throttle, brake, steer) # Commented by Vishnerevsky 22.08.2017
-                self.publish(PID_speed, brake, steer) 
-                #rospy.logwarn("%.2f, %.2f, %.2f" % (self.cur_v, brake, steer))
+
+            #data = [self.last_twist_command, self.current_velocity, self.current_pose, self.final_waypoints]
+            #is_all_data_availabe = all([x is not None for x in data])
+
+            #if self.is_drive_by_wire_enable and is_all_data_availabe:
+            if self.dbw_enabled and self.final_waypoints is not None:
+
+                # Vishnerevsky 22.08.2017:
+                current_time = rospy.get_rostime()
+                current_secs = current_time.secs
+                current_nsecs = current_time.nsecs
+                self.current_timestamp = current_secs + current_nsecs/1000000000.0
+                self.delta_t = (self.current_timestamp - self.previous_timestamp)
+                self.previous_timestamp = self.current_timestamp
+                rospy.logwarn('GO7777!!!')
+
+                # Vishnerevsky 24.08.2017:
+                # Car Velocity Error:
+                VELE = self.final_waypoints[0].twist.twist.linear.x - self.cur_v
+                # Cross Track Error:
+                CTE = self.get_CTE(self.final_waypoints, self.current_pose)
+                if (CTE < -2.0):
+                    CTE = -2.0
+                if (CTE > 2.0):
+                    CTE = 2.0
+                #throttle, brake, steer = self.controller.control(
+                #    VELE, CTE, self.delta_t)
+                rospy.logwarn(CTE)
+                throttle = self.V_pid.step(VELE, self.delta_t)
+                brake = 0
+                if throttle < 0:
+                    brake = throttle
+                    throttle = 0
+                steering = self.S_pid.step(CTE, self.delta_t)
+                self.publish(throttle, brake, steering)
+
+            else:
+                # Here we must reset PID controllers
+                pass
             rate.sleep()
 
     def publish(self, throttle, brake, steer):
@@ -154,54 +157,30 @@ class DBWNode(object):
         bcmd.pedal_cmd = brake
         self.brake_pub.publish(bcmd)
 
+    #def twist_commands_cb(self, msg):
+    #    self.last_twist_command = msg.twist
+
     # Valtgun 20.08.2017 - callback for Drive by wire enabled topic subscriber
     def dbw_cb(self, msg):
         if (msg.data == True):
             self.dbw_enabled = True
         else:
             self.dbw_enabled = False
-        pass
+        if self.dbw_enabled is True:
+            self.V_pid.reset()
+            self.S_pid.reset()
 
-    # Valtgun 20.08.2017 - callback for receiving next waypoint information
-    def waypoints_cb(self, waypoints):
-        # TODO: Takes 10 waypoints ahead, need to make more cleaver follower
-        self.targetX = waypoints.waypoints[10].pose.pose.position.x
-        self.targetY = waypoints.waypoints[10].pose.pose.position.y
-        pass
-
-    # Valtgun 20.08.2017 - callback for receiving next waypoint information
-    def pose_cb(self, msg):
-        self.poseX = msg.pose.position.x
-        self.poseY = msg.pose.position.y
-        orient = msg.pose.orientation
-        X, Y, Z = self.Quaternion_toEulerianAngle(orient.x, orient.y, orient.z, orient.w)
-        self.pose_orient = Z
-        #rospy.logwarn('Pose theta: ' + str(self.pose_orient))
-        # Vishnerevsky 22.08.2017:
-        #self.current_timestamp = msg.header.stamp
-        #rospy.logwarn(self.current_timestamp)
-        pass
-
-    # Vishnerevsky 22.08.2017
-    def twist_cb(self, msg):
-        #rospy.logwarn('twist_cmd received')
-        lin_x = msg.twist.linear.x
-        lin_y = msg.twist.linear.y
-        lin_z = msg.twist.linear.z
-        ang_x = msg.twist.angular.x
-        ang_y = msg.twist.angular.y
-        ang_z = msg.twist.angular.z
-        #rospy.logwarn(lin_x)
-        #rospy.logwarn("%.2f, %.2f, %.2f" % (lin_x, lin_y, lin_z))
-        #rospy.logwarn("%.2f, %.2f, %.2f" % (ang_x, ang_y, ang_z))
-        self.target_speed = lin_x
-        self.target_yaw = 100*ang_z
-        pass
-
+    #def current_velocity_cb(self, msg):
+    #    self.current_velocity = msg.twist
     # Vishnerevsky 22.08.2017
     def cur_vel_cb(self, msg):
         self.cur_v = msg.twist.linear.x
-        pass
+
+    def current_pose_cb(self, msg):
+        self.current_pose = msg.pose
+
+    def final_waypoints_cb(self, msg):
+        self.final_waypoints = msg.waypoints
 
     # Valtgun 20.08.2017 - helper to transform pose quaterion to euler
     # https://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles#Quaternion_to_Euler_Angles_Conversion
@@ -220,6 +199,76 @@ class DBWNode(object):
         t4 = +1.0 - 2.0 * (ysqr + z*z)
         Z = math.degrees(math.atan2(t3, t4))
         return X, Y, Z
+
+    # Vishnerevsky 24.08.2017
+    def get_CTE(self, waypoints, current_pose):
+
+        # We can get points coordinates in this way:
+        #points_x = []
+        #points_y = []
+        #for i in waypoints:
+        #    points_x.append(i.pose.pose.position.x)
+        #    points_y.append(i.pose.pose.position.y)
+
+        # But in this way it can be done faster:
+        points_x = [i.pose.pose.position.x for i in waypoints]
+        points_y = [i.pose.pose.position.y for i in waypoints]
+
+        coeff_xy = list(reversed(np.polyfit(points_x, points_y, 2)))
+        coeff_yx = list(reversed(np.polyfit(points_y, points_x, 2)))
+
+        dist_y = 0
+        for p, coeff in enumerate(coeff_xy):
+            dist_y += coeff * (current_pose.position.x ** p)    
+        b = math.fabs(current_pose.position.y - dist_y)
+        dist_x = 0
+        for p, coeff in enumerate(coeff_xy):
+            dist_x += coeff * (current_pose.position.y ** p)  
+        a = math.fabs(current_pose.position.x - dist_x)
+        # Vishnerevsky 24.08.2017
+        # We can add my geometry pictures to write up
+        beta = math.atan2(a,b)
+        alfa = math.atan2(b,a)
+
+        c1 = b * math.cos(beta)
+        c2 = a * math.cos(alfa)
+
+        d1 = math.sqrt(b*b - c1*c1)
+        d2 = math.sqrt(a*a - c2*c2)
+        d = (d1 + d2)/2.0
+
+        # From quaternion to Euler angles:
+        x = current_pose.orientation.x
+        y = current_pose.orientation.y
+        z = current_pose.orientation.z
+        w = current_pose.orientation.w
+
+        # This is not really necessary:
+        #ysqr = y*y
+        #t0 = +2.0 * (w * x + y*z)
+        #t1 = +1.0 - 2.0 * (x*x + ysqr)
+        #X = math.degrees(math.atan2(t0, t1))
+
+        #t2 = +2.0 * (w*y - z*x)
+        #t2 =  1 if t2 > 1 else t2
+        #t2 = -1 if t2 < -1 else t2
+        #Y = math.degrees(math.asin(t2))
+
+        t3 = +2.0 * (w * z + x*y)
+        t4 = +1.0 - 2.0 * (y*y + z*z)
+        Z = math.degrees(math.atan2(t3, t4))
+
+        theta = Z
+
+        # Get one waypoint:
+        Xmap = waypoints[10].pose.pose.position.x
+        Ymap = waypoints[10].pose.pose.position.y
+        # Transform vaypoint coordinate to the car coordinate system:
+        #Xcar = (Ymap-current_pose.position.y)*math.sin(math.radians(theta))-(current_pose.position.x-Xmap)*math.cos(math.radians(theta))
+        Ycar = (Ymap-current_pose.position.y)*math.cos(math.radians(theta))-(Xmap-current_pose.position.x)*math.sin(math.radians(theta))
+        if (Ycar < 0):
+            d = d * -1.0
+        return d
 
 
 if __name__ == '__main__':
