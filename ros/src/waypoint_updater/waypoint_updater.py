eb2ca584b0afb1767482179422f344b2017a372c
Omarhany <omarhanysalah@gmail.com>
2017-08-25 16:31:24 +0200

    Update waypoint_updater.py

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -3,7 +3,7 @@
 import math
 import rospy
 from geometry_msgs.msg import PoseStamped
-from styx_msgs.msg import Lane, Waypoint
+from styx_msgs.msg import Lane, Waypoint, TrafficLightArray
 import std_msgs.msg
 
 
@@ -32,6 +32,14 @@ class WaypointUpdater(object):
         rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
 
         # TODO: Add a subscriber for /traffic_waypoint and /obstacle_waypoint below
+
+		# Omar 25.08.2017 Subscribed to Traffic lights waypoints
+        rospy.Subscriber('/vehicle/traffic_lights', TrafficLightArray, self.traffic_cb)
+		# Omar 25.08.2017 add a variable for the car current position
+		self.pose = None
+        # Omar 25.08.2017 add a variable lane
+		self.lane = Lane()
+
         self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
 
         # TODO: Add other member variables you need below
@@ -45,11 +53,10 @@ class WaypointUpdater(object):
         # TODO: Implement
         if self.last_waypoints is not None:
             #rospy.logwarn('GO_555!!!')
-            pose = msg.pose
+            self.pose = msg.pose
             waypoints = self.last_waypoints.waypoints
-            lane = Lane()
             lane.header.stamp = rospy.Time.now()
-            start_point = self.closest_waypoint(pose, waypoints)
+            start_point = self.closest_waypoint(self.pose, waypoints)
             all_waypoints = waypoints + waypoints[:LOOKAHEAD_WPS]
             lane.waypoints = all_waypoints[start_point-self.points_in_back: start_point-self.points_in_back + LOOKAHEAD_WPS]         
 
@@ -65,17 +72,29 @@ class WaypointUpdater(object):
 
     def traffic_cb(self, msg):
         # TODO: Callback for /traffic_waypoint message. Implement
-        pass
+		# Omar 25.08.2017 Calculate the closest Waypoint to the current car position
+		Closest_TrafficLight = self.closest_waypoint(pose, msg.lights)
+		# Omar 25.08.2017 Check if the closest Traffic light is red
+        if msg.lights[Closest_TrafficLight].state == 0:
+	    # Omar 25.08.2017 Find the waypoints between the car and the Traffic Sign and set their velocity to 0
+			for index in range(len(lane.waypoints)):
+				if self.distance(pose.position, lane.waypoints[index].pose.pose.position) < self.distance(pose.position, msg.lights[Closest_TrafficLight].pose.pose.position):
+                    self.set_waypoint_velocity(lane.waypoints, index, 0)
+                	
+     
 
     def obstacle_cb(self, msg):
         # TODO: Callback for /obstacle_waypoint message. We will implement it later
         pass
 
+    # Omar 25.08.2017 Get the waypoint velocity
     def get_waypoint_velocity(self, waypoint):
-        pass
-
+	return waypoint.twist.twist.linear.x
+        
+    # Omar 25.08.2017 Set the waypoint velocity
     def set_waypoint_velocity(self, waypoints, waypoint, velocity):
-        pass
+	waypoints[waypoint].twist.twist.linear.x = velocity
+        
 
     def closest_waypoint(self, pose, waypoints):
         best_waypoint = 0
